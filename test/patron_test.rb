30ee8451bc6d91591545eb9c72f4b8176a31c0c8
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-10 09:17:24 -0600

    Create methods to test access to attributes and create Patron class

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -10,4 +10,22 @@ class PatronTest < Minitest::Test
     assert_instance_of Patron, patron_1
   end
 
+  def test_it_has_a_name
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal "Bob", patron_1.name
+  end
+
+  def test_it_has_spending_money
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal 20, patron_1.spending_money
+  end
+
+  def test_it_initially_has_no_interests
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal [], patron_1.interests
+  end
+
 end
